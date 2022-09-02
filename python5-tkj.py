7d3bba42781b55f1208d709ce5845afac62a939c
yudhanty <112373606+yudhanty@users.noreply.github.com>
2022-09-01 20:25:04 -0700

    Add files via upload

---
+++ python5-tkj.py
@@ -0,0 +1,39 @@
+#RUMUS PERSEGI SISI * SISI
+
+sisi1 = 20
+sisi2 =30
+luas_p = sisi1 * sisi2
+
+print ("luas persegi adalah",luas_p)
+
+# RUMUS SEGITIGA A*T/2
+
+alas = 20
+tinggi = 10
+luas_s = alas * tinggi/2
+
+print ("luas segitiga adalh",luas_s)
+
+#RUMUS JAJAR GENJANG ALAS * TINGGI
+
+alas = 20
+tinggi = 10
+luas_j =alas * tinggi
+
+print ("luas jajar genjang adalh",luas_j)
+
+#RUMUS LINGKARAN phi * r * r
+
+phi = 22/7
+r = 15
+luas_1 = phi * r * r
+
+print ("luas lingkaran adalah", luas_1)
+
+''' PEROGRAM DINAMIS '''
+
+print ("\n1.luas persegi")
+sisi = int(input("masukan nilai sisi") )
+luas_p =sisi * sisi
+print ("luas persegi adalh ",luas_p)
+
