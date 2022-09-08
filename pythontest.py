66ba8772e8f1baa23298f3232af2f3f373f4c07b
yudhanty <112373606+yudhanty@users.noreply.github.com>
2022-09-07 23:44:25 -0700

    Add files via upload

---
+++ pythontest.py
@@ -0,0 +1,28 @@
+# Persegi
+sisi = int(input("masukkan nilai sisi"))
+luas_p = sisi * sisi
+print("luas persegi adalah", luas_p)
+
+# persegi panjang
+panjang = int(input("masukkan nilai panjang"))
+lebar = int(input("masukkan nilai lebar"))
+luas_pj = panjang * lebar
+print("luas persegi panjang adalah", luas_pj)
+
+# jajar genjang
+alas = int(input("masukkan nilai alas"))
+tinggi = int(input("masukkan nilai tinggi"))
+luas_jg = alas * tinggi
+print("luas jajar genjang adalah", luas_jg)
+
+# Lingkaran
+r = int(input("masukkan nilai r"))
+phi = 22/7
+luas_l = phi * r * r
+print("luas lingkaran adalah", luas_l)
+
+# Segitiga
+alas = int(input("masukkan nilai alas"))
+tinggi = int(input("masukkan nilai tinggi"))
+luas_s = 1/2 * alas * tinggi
+print("luas segitiga adalah", luas_s)
